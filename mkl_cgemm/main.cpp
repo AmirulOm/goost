22607c45cb52b317f4a5281d26b7913d8bcef385
AmirulOm <amirul.abdullah89@gmail.com>
2018-08-29 16:37:12 +0800

    edit cgemm

--- mkl_cgemm/main.cpp
+++ mkl_cgemm/main.cpp
@@ -8,7 +8,6 @@ int main()
 {
 	int row_a = 2;
 	int col_a = 3;
-
 	int row_b = 3;
 	int col_b = 4;
 
@@ -24,57 +23,37 @@ int main()
 
 	/*
 	Store in column base
-	[ 1 2 3 ]
-	[ 4 5 6 ]
+	[ 1+i 2+i 3+i ]
+	[ 4+i 5+i 6+i ]
 	*/
 
-	a[0].real = 1;
-	a[1].real = 4;
-	a[2].real = 2;
-	a[3].real = 5;
-	a[4].real = 3;
-	a[5].real = 6;
-	a[0].imag = 1;
-	a[1].imag = 4;
-	a[2].imag = 2;
-	a[3].imag = 5;
-	a[4].imag = 3;
-	a[5].imag = 6;
+	a[0].real = 1; a[0].imag = 1;
+	a[1].real = 4; a[1].imag = 4;
+	a[2].real = 2; a[2].imag = 2;
+	a[3].real = 5; a[3].imag = 5;
+	a[4].real = 3; a[4].imag = 3;
+	a[5].real = 6; a[5].imag = 6;
 	
 	/*
 	Store in row based
-	[ 1  2  3  4 ]
-	[ 5  6  7  8 ]
-	[ 9 10 11 12 ]
+	[ 1+i  2+i  3+i  4+i ]
+	[ 5+i  6+i  7+i  8+i ]
+	[ 9+i 10+i 11+i 12+i ]
 	*/
 
-	b[0].real = 1;
-	b[1].real = 2;
-	b[2].real = 3;
-	b[3].real = 4;
-	b[4].real = 5;
-	b[5].real = 6;
-	b[6].real = 7;
-	b[7].real = 8;
-	b[8].real = 9;
-	b[9].real = 10;
-	b[10].real = 11;
-	b[11].real = 12;
-	b[0].imag = 1;
-	b[1].imag = 2;
-	b[2].imag = 3;
-	b[3].imag = 4;
-	b[4].imag = 5;
-	b[5].imag = 6;
-	b[6].imag = 7;
-	b[7].imag = 8;
-	b[8].imag = 9;
-	b[9].imag = 10;
-	b[10].imag = 11;
-	b[11].imag = 12;
-
+	b[0].real  = 1;  b[0].imag = 1;
+	b[1].real  = 2;  b[1].imag = 2;
+	b[2].real  = 3;  b[2].imag = 3;
+	b[3].real  = 4;  b[3].imag = 4;
+	b[4].real  = 5;  b[4].imag = 5;
+	b[5].real  = 6;  b[5].imag = 6;
+	b[6].real  = 7;  b[6].imag = 7;
+	b[7].real  = 8;  b[7].imag = 8;
+	b[8].real  = 9;  b[8].imag = 9;
+	b[9].real  = 10; b[9].imag = 10;
+	b[10].real = 11; b[10].imag = 11;
+	b[11].real = 12; b[11].imag = 12;
 	
-
 	cblas_cgemm(CblasColMajor,
 				CblasNoTrans,   		//const CBLAS_TRANSPOSE transa,
 				CblasTrans,				//const CBLAS_TRANSPOSE transb,
